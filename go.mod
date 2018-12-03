8fc8951d731214d85d89907e740a68109ceb5f14
Leandro Barbosa <leandrobar93@gmail.com>
2018-12-03 19:11:25 -0200

    init project

---
+++ go.mod
@@ -0,0 +1,3 @@
+module .
+
+require google.golang.org/grpc v1.16.0 // indirect
---
+++ greet_server/server.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+
+	. "github.com/Leeaandrob/grpc-golang/greet/greetpb"
+
+	"google.golang.org/grpc"
+)
+
+type server struct {
+}
+
+func main() {
+	fmt.Println("vim-go")
+
+	lis, err := net.Listen("tcp", "0.0.0.0:50051")
+
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	greetpb.RegisterGreetServiceServer(s, &server{})
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to server: %v", err)
+	}
+
+}
